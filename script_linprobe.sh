6f8f68dff4bdec19faac0e05da4d8c6fb117f9e4
YinxPeng <144885959+YinxPeng@users.noreply.github.com>
2024-09-25 20:59:59 +0800

    Update script_linprobe.sh

--- script_linprobe.sh
+++ script_linprobe.sh
@@ -6,95 +6,49 @@ python -m torch.distributed.launch --nproc_per_node=2 --master_port 12345 main_l
 --config ./config/ntu60_xsub_joint/linprobe.yaml \
 --output_dir ./output_dir/ntu60_xsub_joint/linear \
 --log_dir ./output_dir/ntu60_xsub_joint/linear \
---finetune ./output_dir/ntu60_xsub_joint/pretrain_mamp+3_mask90_tau0.80_ep400_noamp/checkpoint-399.pth \
+--finetune ./output_dir/ntu60_xsub_joint/pretrain/checkpoint-399.pth \
 --dist_eval
-
 
 
 # NTU-60 xview
 python -m torch.distributed.launch --nproc_per_node=2 --master_port 12345 main_linprobe.py \
 --config ./config/ntu60_xview_joint/linprobe.yaml \
---output_dir ./output_dir/ntu60_xview_joint/linear_mae+3_mask90_ep400_400 \
---log_dir ./output_dir/ntu60_xview_joint/linear_mae+3_mask90_ep400_400 \
---finetune ./output_dir/ntu60_xview_joint/pretrain_mae+3_mask90_ep400_noamp/checkpoint-399.pth \
---dist_eval
-
-# NTU-60 xsub
-python -m torch.distributed.launch --nproc_per_node=2 --master_port 12345 main_linprobe.py \
---config ./config/ntu60_xview_joint/linprobe.yaml \
 --output_dir ./output_dir/ntu60_xview_joint/linear \
 --log_dir ./output_dir/ntu60_xview_joint/linear \
---finetune ./output_dir/ntu60_xview_joint/pretrain_mamp+3_mask90_tau0.80_ep400_noamp/checkpoint-399.pth \
+--finetune ./output_dir/ntu60_xview_joint/pretrain/checkpoint-399.pth \
 --dist_eval
-
 
 
 # NTU-120 xset
 python -m torch.distributed.launch --nproc_per_node=2 --master_port 12345 main_linprobe.py \
 --config ./config/ntu120_xset_joint/linprobe.yaml \
---output_dir ./output_dir/ntu120_xset_joint/linear_mae+5_mask90_ep400_400 \
---log_dir ./output_dir/ntu120_xset_joint/linear_mae+5_mask90_ep400_400 \
---finetune ./output_dir/ntu120_xset_joint/pretrain_mae+5_mask90_ep400_noamp/checkpoint-399.pth \
---dist_eval
-
-# NTU-120 xset
-python -m torch.distributed.launch --nproc_per_node=2 --master_port 12345 main_linprobe.py \
---config ./config/ntu120_xset_joint/linprobe.yaml \
---output_dir ./output_dir/ntu120_xset_joint/linear_mamp+5_mask90_tau0.75_ep400_400 \
---log_dir ./output_dir/ntu120_xset_joint/linear_mamp+5_mask90_tau0.75_ep400_400 \
---finetune ./output_dir/ntu120_xset_joint/pretrain_mamp+5_mask90_tau0.75_ep400_noamp/checkpoint-399.pth \
---dist_eval
-
-
-
-# NTU-120 xsub
-python -m torch.distributed.launch --nproc_per_node=2 --master_port 12345 main_linprobe.py \
---config ./config/ntu120_xsub_joint/linprobe.yaml \
---output_dir ./output_dir/ntu120_xsub_joint/linear_mae+5_mask90_ep400_400 \
---log_dir ./output_dir/ntu120_xsub_joint/linear_mae+5_mask90_ep400_400 \
---finetune ./output_dir/ntu120_xsub_joint/pretrain_mae+5_mask90_ep400_noamp/checkpoint-399.pth \
+--output_dir ./output_dir/ntu120_xset_joint/linear \
+--log_dir ./output_dir/ntu120_xset_joint/linear \
+--finetune ./output_dir/ntu120_xset_joint/pretrain/checkpoint-399.pth \
 --dist_eval
 
 # NTU-120 xsub
 python -m torch.distributed.launch --nproc_per_node=2 --master_port 12345 main_linprobe.py \
 --config ./config/ntu120_xsub_joint/linprobe.yaml \
---output_dir ./output_dir/ntu120_xsub_joint/linear_mamp+5_mask90_tau0.75_ep400_400 \
---log_dir ./output_dir/ntu120_xsub_joint/linear_mamp+5_mask90_tau0.75_ep400_400 \
---finetune ./output_dir/ntu120_xsub_joint/pretrain_mamp+5_mask90_tau0.75_ep400_noamp/checkpoint-399.pth \
+--output_dir ./output_dir/ntu120_xsub_joint/linear \
+--log_dir ./output_dir/ntu120_xsub_joint/linear \
+--finetune ./output_dir/ntu120_xsub_joint/pretrain/checkpoint-399.pth \
 --dist_eval
-
 
 
 # PKU Phase I
 python -m torch.distributed.launch --nproc_per_node=2 --master_port 12345 main_linprobe.py \
 --config ./config/pkuv1_xsub_joint/linprobe.yaml \
---output_dir ./output_dir/pkuv1_xsub_joint/linear_mae+5_mask90_ep400_300 \
---log_dir ./output_dir/pkuv1_xsub_joint/linear_mae+5_mask90_ep400_300 \
---finetune ./output_dir/pkuv1_xsub_joint/pretrain_mae+5_mask90_ep400_noamp/checkpoint-300.pth \
---dist_eval
-
-# PKU Phase I
-python -m torch.distributed.launch --nproc_per_node=2 --master_port 12345 main_linprobe.py \
---config ./config/pkuv1_xsub_joint/linprobe.yaml \
---output_dir ./output_dir/pkuv1_xsub_joint/linear_mamp+5_mask90_tau0.80_ep400_300 \
---log_dir ./output_dir/pkuv1_xsub_joint/linear_mamp+5_mask90_tau0.80_ep400_300 \
---finetune ./output_dir/pkuv1_xsub_joint/pretrain_mamp+5_mask90_tau0.80_ep400_noamp/checkpoint-300.pth \
+--output_dir ./output_dir/pkuv1_xsub_joint/linear \
+--log_dir ./output_dir/pkuv1_xsub_joint/linear \
+--finetune ./output_dir/pkuv1_xsub_joint/pretrain/checkpoint-399.pth \
 --dist_eval
 
 
-
-# PKU Phase I
+# PKU Phase II
 python -m torch.distributed.launch --nproc_per_node=2 --master_port 12345 main_linprobe.py \
 --config ./config/pkuv2_xsub_joint/linprobe.yaml \
---output_dir ./output_dir/pkuv2_xsub_joint/linear_mae+5_mask90_ep400_300 \
---log_dir ./output_dir/pkuv2_xsub_joint/linear_mae+5_mask90_ep400_300 \
---finetune ./output_dir/pkuv2_xsub_joint/pretrain_mae+5_mask90_ep400_noamp/checkpoint-300.pth \
---dist_eval
-
-# PKU Phase I
-python -m torch.distributed.launch --nproc_per_node=2 --master_port 12345 main_linprobe.py \
---config ./config/pkuv2_xsub_joint/linprobe.yaml \
---output_dir ./output_dir/pkuv2_xsub_joint/linear_mamp+5_mask90_tau0.80_ep400_300 \
---log_dir ./output_dir/pkuv2_xsub_joint/linear_mamp+5_mask90_tau0.80_ep400_300 \
---finetune ./output_dir/pkuv2_xsub_joint/pretrain_mamp+5_mask90_tau0.80_ep400_noamp/checkpoint-300.pth \
+--output_dir ./output_dir/pkuv2_xsub_joint/linear \
+--log_dir ./output_dir/pkuv2_xsub_joint/linear \
+--finetune ./output_dir/pkuv2_xsub_joint/pretrain/checkpoint-399.pth \
 --dist_eval
